87593aad177be5113b351edf3f7c15cd8b8619e1
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-04-19 15:52:48 +0300

    Create page 3 card

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import community from "../assets/images/community.png";
+import connect from "../assets/images/connect.png";
+import delivery from "../assets/images/page3delivery.jpg";
+import waste from "../assets/images/waste.png";
+import variety from "../assets/images/variety.png";
 
 function Section2() {
-  return;
+  return (
+    <div className="page3-cards">
+      <div className="page3-card">
+        <div className="page3-text">
+          <h2>Build a community of like minded people</h2>
+          <p>
+            Connect with others who share your value and interests through items
+            exchange, creating a community centered around sustainability and
+            reducing waste
+          </p>
+          <img src={community} />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Section2;
