8c02168c3c6f7603777375e4fe1d2dda65f703f8
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-04-19 15:27:09 +0300

    Remove unnecessary stylings

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 
 function Section2() {
-  return (
-    <h2>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo tempore
-      quas distinctio minima nobis cumque dolores blanditiis. Corporis libero
-      nisi nostrum, impedit quisquam accusamus reiciendis eaque quia molestiae
-      aut minima.
-    </h2>
-  );
+  return;
 }
 
 export default Section2;
