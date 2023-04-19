15c1423dac51d67a26ee729234c2e773874921e1
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-04-19 16:10:38 +0300

    Remove unnecessary images

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,6 +1,6 @@
 import community from "../assets/images/community.png";
 import connect from "../assets/images/connect.png";
-import delivery from "../assets/images/page3delivery.jpg";
+import delivery from "../assets/images/delivery2.png";
 import waste from "../assets/images/waste.png";
 import variety from "../assets/images/variety.png";
 
@@ -15,8 +15,52 @@ function Section2() {
             exchange, creating a community centered around sustainability and
             reducing waste
           </p>
-          <img src={community} />
         </div>
+        <img src={community} />
+      </div>
+      <div className="page3-card">
+        <div className="page3-text">
+          <h2>Connect with your community</h2>
+          <p>
+            Lewach helps with you connect with your local community by
+            exchanging items with people nearby. Suppor emall businesses and
+            meet new people without leavinf the comfort of your own home
+          </p>
+        </div>
+        <img src={connect} />
+      </div>
+      <div className="page3-card">
+        <div className="page3-text">
+          <h2>REDUCE WASTE</h2>
+          <p>
+            By exchanging items with others, you can reduce waste and help the
+            environment. Lewach encourages sustainability and supports the
+            sharing economy.
+          </p>
+        </div>
+        <img src={waste} />
+      </div>
+      <div className="page3-card">
+        <div className="page3-text">
+          <h2>Convenient delivery service</h2>
+          <p>
+            Our delivery service makes it easy to exchange items with others in
+            your community. You can arrange for pick-up and delivery of items,
+            saving you time and hassle.
+          </p>
+        </div>
+        <img src={delivery} />
+      </div>
+      <div className="page3-card">
+        <div className="page3-text">
+          <h2>increase variety in your life</h2>
+          <p>
+            Find new items to enjoy and explore through item exchange, including
+            unique and interesting pieces that might not have otherwise
+            discovered.
+          </p>
+        </div>
+        <img src={variety} />
       </div>
     </div>
   );
