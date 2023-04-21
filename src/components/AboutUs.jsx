f6a9bd20b1dc61fbf831a6a044344c9ad0150294
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-04-22 01:45:05 +0300

    Complete about us page

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import aboutus from "../assets/images/aboutus.jpg";
 
 function AboutUs() {
-  return <div>AboutUs</div>;
+  return (
+    <div className="aboutus-root">
+      <div className="aboutus-text">
+        <h1>ABOUT US</h1>
+        <p>
+          We are a community-driven platform committed to offering a seamless
+          and secure experience for buying and selling already pre-owned items,
+          swapping industrial, electronic, and mechanical supplies. We take
+          pride in our commitment to integrity, diversity, and innovation, and
+          we’re always looking to improve our services to meet the evolving
+          needs of our valued customers. Thank you for choosing us as your go-to
+          exchange website, and we look forward to serving you.
+        </p>
+      </div>
+      <div className="aboutus-img">
+        <img src={aboutus} />
+      </div>
+    </div>
+  );
 }
 
 export default AboutUs;
