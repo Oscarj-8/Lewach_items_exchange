67d544a0451b09143e67478f4552b1aee33ab1b3
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-04-22 02:04:33 +0300

    Create last section

---
+++ src/components/LastSec.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function LastSec() {
+  return (
+    <div className="last-sec">
+      <span>
+        Are you tired of clutter and ready to swap your gently used items for
+        something new and exciting?
+      </span>
+      <button className="btn2">REGISTER NOW</button>
+    </div>
+  );
+}
+
+export default LastSec;
