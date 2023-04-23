d318ef2ef5464b2112fae3bd15ed8641b79da03a
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-04-23 15:35:23 +0300

    Install fontawesome

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 import footerLogo from "../assets/images/footerLogo.png";
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 function Footer() {
   return (
     <div className="footer-root">
@@ -7,13 +12,26 @@ function Footer() {
         <div className="footer-logo">
           <img src={footerLogo} alt="Lewach logo" />
         </div>
-        <div className="footer-teext">
+        <div className="footer-text">
           <p>
             Lewach is an items exchange website that users can exchange their
             used items with each other. for more read about us section
           </p>
+          <div className="footer-social-icons">
+            <FontAwesomeIcon
+              icon={faFacebook}
+              className="facebook-icon social-icons"
+            />
+            <FontAwesomeIcon
+              icon={faTwitter}
+              className="twitter-icon social-icons"
+            />
+            <FontAwesomeIcon
+              icon={faInstagram}
+              className="instagram-icon social-icons"
+            />
+          </div>
         </div>
-        <div className="footer-social-icons"></div>
       </div>
       <div className="footer-center"></div>
       <div className="footer-right"></div>
