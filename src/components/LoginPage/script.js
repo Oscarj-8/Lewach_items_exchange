04bea9476a01d179614a58126d1ba4d9d35bca42
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-05-08 21:56:42 +0300

    Add js file

--- src/components/LoginPage/script.js
+++ src/components/LoginPage/script.js
@@ -1,11 +1,11 @@
-const signUpButton = document.querySelector('.overlay__button-sign-up');
-const signInButton = document.querySelector('.overlay__button-sign-in');
-const container = document.querySelector('.container');
+const signUpButton = document.querySelector(".overlay__button-sign-up");
+const signInButton = document.querySelector(".overlay__button-sign-in");
+const container = document.querySelector(".container");
 
-signUpButton.addEventListener('click', () => {
-    container.classList.add('sign-out-panel-active');
+signUpButton.addEventListener("click", () => {
+  container.classList.add("sign-out-panel-active");
 });
 
-signInButton.addEventListener('click', () => {
-    container.classList.remove('sign-out-panel-active');
+signInButton.addEventListener("click", () => {
+  container.classList.remove("sign-out-panel-active");
 });
