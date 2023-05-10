a073a462dbf2b1749f9238fe75ef1e22d685cdbb
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-05-10 10:45:17 +0300

    Create phone header component

--- src/components/HomePage/Home_page_header.jsx
+++ src/components/HomePage/Home_page_header.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import "./homePage.css";
+import logo from "../../assets/images/logoWhite.png";
+import menu from "../../assets/images/phone-menu.png";
 
 function Home_page_header() {
-  return <div className="home-page-header"></div>;
+  return (
+    <div className="home-page-header">
+      <img src={logo} />
+      <img src={menu} className="phone-menu" />
+    </div>
+  );
 }
 
 export default Home_page_header;
