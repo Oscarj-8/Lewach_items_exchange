8c9bfe78399d22c161dcec150a723fefab1c8468
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-05-11 00:13:08 +0300

    Done section 1

--- src/components/HomePage/HomePage_section1.jsx
+++ src/components/HomePage/HomePage_section1.jsx
@@ -1,7 +1,28 @@
-import React from "react";
+import { FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight } from "react-icons/fa";
 
 function HomePage_section1() {
-  return <div>homePage_section1</div>;
+  return (
+    <div className="homePage-section1">
+      <FaChevronLeft size="2em" />
+      <div className="section1-card">
+        <div className="section1-card-top">
+          <div className="card-top-text">
+            <h2>Asus laptop</h2>
+            <span>
+              Body Medium Lorem ipsum dolor sit amet consectetur cambuur.
+            </span>
+          </div>
+          <div className="card-top-img"></div>
+        </div>
+        <div className="section1-card-bottom">
+          <button className="card-bottom-btn">Trade</button>
+          <button className="card-bottom-btn">Contact</button>
+        </div>
+      </div>
+      <FaChevronRight size="2em" />
+    </div>
+  );
 }
 
 export default HomePage_section1;
