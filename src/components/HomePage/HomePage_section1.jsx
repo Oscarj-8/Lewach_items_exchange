4375c48850a6bc6e97a5ee3ce54757bc5fe13711
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-05-13 01:00:07 +0300

    Add items card display to section 1

--- src/components/HomePage/HomePage_section1.jsx
+++ src/components/HomePage/HomePage_section1.jsx
@@ -6,21 +6,6 @@ function HomePage_section1() {
   return (
     <div className="homePage-section1">
       <FaChevronLeft size="2em" />
-      {/* <div className="section1-card">
-        <div className="section1-card-top">
-          <div className="card-top-text">
-            <h2>Asus laptop</h2>
-            <span>
-              Body Medium Lorem ipsum dolor sit amet consectetur cambuur.
-            </span>
-          </div>
-          <div className="card-top-img"></div>
-        </div>
-        <div className="section1-card-bottom">
-          <button className="card-bottom-btn">Trade</button>
-          <button className="card-bottom-btn">Contact</button>
-        </div>
-      </div> */}
       <ItemsCard />
       <FaChevronRight size="2em" />
     </div>
