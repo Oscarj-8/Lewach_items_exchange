fcc5b68b7196b4859b5a13cc9afee2f50a00b4ee
Biniyam Zeleke <58305735+Binize@users.noreply.github.com>
2023-05-19 19:45:43 +0300

    backend with mongodb

--- src/components/LoginPage/Logreg.jsx
+++ src/components/LoginPage/Logreg.jsx
@@ -1,23 +1,52 @@
 import logo from "../../assets/images/logoBlue.png";
 import "../../logreg.css";
 import React, { useState } from "react";
-import Validation from "./LoginValidation";
+import"../../backend/server"
 
 function Logreg() {
-  const [values, setValues] = useState({
-    email: "",
-    password: ""
-  })
-  const [errors, setErrors] = useState({
-  })
-  const handleInput = (event) => {
-    setValues(prev => ({...prev, [event.target.name]: [event.target.values]}));
-  }
+  const handleSubmit = async () => {
+    const email = document.querySelector('input[name="email"]').value;
+    const password = document.querySelector('input[name="password"]').value;
 
-  const handleSubmit = (event) => {
+    const response = await fetch('http://localhost:3001/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (response.ok) {
+      const user = await response.json();
+      console.log('Logged in:', user);
+    } else {
+      console.error('Error:', response.statusText);
+    }
+  };
+
+  const handleRegister = async (event) => {
     event.preventDefault();
-    setErrors(Validation(values));
-  }
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    const response = await fetch('http://localhost:3001/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    });
+   
+    if (response.ok) {
+      const newUser = await response.json();
+      console.log('Registered:', newUser);
+    } else {
+      console.error('Error:', response.statusText);
+    }
+  };
+
+
 
   function adder(){
     const container = document.querySelector('.container');
@@ -69,45 +98,45 @@ function next(){
         </div>
       </section>
       <section className="container-form container-form-sign-up">
-        <div className="sign-up-form">
+        <form method="post" className="sign-up-form" >
           <div className="form first">
           <h1>Create Account</h1>
           <div className="fl">
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
+              <input type="text" placeholder="First Name" name="firstname" required={true} />
+            <input type="text" placeholder="Last Name" name="lastname" required={true}/>
           </div>
-          <input type="text" placeholder=" Username" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="password" placeholder="Confirm Password" />
+          <input type="text" placeholder=" Username" name="username" required={true}/>
+          <input type="email" placeholder="Email" name="email" required={true}/>
+          <input type="password" placeholder="Password" name="password" required={true}/>
+          <input type="password" placeholder="Confirm Password" name="confirmpassword" required={true}/>
           <button onClick={next} className="nextBtn">Next</button>
           </div> 
 
-         <div className="form second">
+         <div className="form second" >
           <h1>Address</h1> 
             <div >
-              <select name="region" id="region">
+              <select name="region" id="region" required={true}>
               <option value="" disabled selected hidden>Region</option>
                 <option>A</option>
                 <option>B</option>
                 <option>C</option>
               </select>
-              <select name="region" id="region">
+              <select name="cityzone" id="cityzone" required={true}>
               <option value="" disabled selected hidden>City/Zone</option>
                 <option>A</option>
                 <option>B</option>
                 <option>C</option>
               </select>
-          <input type="text" placeholder=" Subcity/Woreda" />
-          <input type="text" placeholder="P.O.Box" />
-          <input type="text" placeholder="Special Name for your location" />
-          <input type="text" placeholder="SSN" />
+          <input type="text" placeholder=" Subcity/Woreda" name="subcityworeda" required={true}/>
+          <input type="text" placeholder="P.O.Box" name="pobox"/>
+          <input type="text" placeholder="Special Name for your location" name="snl"/>
+          <input type="text" placeholder="SSN" name="ssn"/>
            <input type="checkbox" name="" id="" /> 
           <button onClick={back} className="backtBtn">Back</button>
           <button>Register</button>
           </div>
           </div>
-        </div>
+        </form>
       </section>
       <section>
       </section>
