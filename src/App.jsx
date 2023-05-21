9369b08c8e857d0b4cfe4b9ddd387227717770cd
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-05-21 22:22:54 +0300

    Last one

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,10 @@
 import "./App.css";
+import FullHomepage from "./components/HomePage/FullHomepage";
 import FullLandingPage from "./components/LandingPage/FullLandingPage";
-import Home_page_header from "./components/HomePage/Home_page_header";
-import Search_menu from "./components/HomePage/Search_menu";
-import HomePage_section1 from "./components/HomePage/HomePage_section1";
-import HomePage_section2 from "./components/HomePage/HomePage_section2";
-import Footer from "./components/LandingPage/Footer";
 function App() {
   return (
     <div className="root">
-      {/* <FullLandingPage /> */}
-      <Home_page_header />
-      <Search_menu />
-      <HomePage_section1 />
-      <HomePage_section2 />
-      <Footer />
+      <FullLandingPage />
     </div>
   );
 }
