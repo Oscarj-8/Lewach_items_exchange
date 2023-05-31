9d39c2afd19b51c942892cfe6f4a5a78729d38ae
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-05-31 10:38:37 +0300

    Done dashboad section

--- src/components/AdminPage/AdminPageMain.jsx
+++ src/components/AdminPage/AdminPageMain.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AdminPageHeader from "./AdminPageHeader";
+import AdminMainSectionOne from "./AdminMainSectionOne";
 
 function AdminPageMain() {
   return (
     <div className="adminPage-main">
       <AdminPageHeader />
+      <AdminMainSectionOne />
     </div>
   );
 }
