81b814f9d8b3f8e0ccc47cfec7b13ab81f8d3f21
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-06-03 10:29:58 +0300

    Finish the table style

--- src/components/AdminPage/AdminPage.jsx
+++ src/components/AdminPage/AdminPage.jsx
@@ -3,13 +3,15 @@ import Sidebar from "./Sidebar";
 import AdminDashboard from "./AdminDashboard";
 import SearchBar from "./SearchBar";
 import AdminDeliveryRequest from "./AdminDeliveryRequest";
+import ManageAccount from "./ManageAccount";
 function AdminPage() {
   return (
     <div className="adminPage-root">
       <Sidebar />
       <SearchBar />
       {/* <AdminDashboard /> */}
-      <AdminDeliveryRequest />
+      {/* <AdminDeliveryRequest /> */}
+      <ManageAccount />
     </div>
   );
 }
