1aedd0e4af45a8c7535db5552436d08098a947d7
Oscar Junior <mmmabdulahi954@yahoo.com>
2023-06-08 01:08:04 +0300

    Add modal for editing user profile

--- src/components/HomePage/UserInformationSection.jsx
+++ src/components/HomePage/UserInformationSection.jsx
@@ -1,5 +1,5 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLongArrowAltLeft } from "@fortawesome/free-solid-svg-icons";
+import { faLongArrowAltLeft, faClose } from "@fortawesome/free-solid-svg-icons";
 import UploadModal from "./UploadModal";
 import { useState } from "react";
 
@@ -26,7 +26,10 @@ function UserInformationSection() {
             <hr />
             <span>Email : LilaSis23@gmail.com</span>
             <hr />
-            <span>Password : ************</span>
+            <div className="password-sec">
+              <span>Password : ************</span>
+              <button>change password</button>
+            </div>
           </div>
           <div className="userInfo-2">
             <h2>Address information</h2>
@@ -41,42 +44,53 @@ function UserInformationSection() {
           <div className="userProfile-editBtn">
             <button onClick={() => setModalIsOpen(true)}>Edit</button>
             <UploadModal isOpen={modalIsOpen} onRequestClose={closeModal}>
-              <div className="persomal-info-edit">
-                <div className="personal-info-edit">
-                  <div>
-                    <span>First name : </span>
-                    <input type="text" />
-                  </div>
-                  <div>
-                    <span>Last name : </span>
-                    <input type="text" />
-                  </div>
-                  <div>
-                    <span>Email : </span>
-                    <input type="text" />
-                  </div>
-                  <div>
-                    <span>Password: </span>
-                    <input type="text" />
+              <div className="personal-info-edit">
+                <div className="personalInfo-closeBtn">
+                  <h2>User Information</h2>
+                  <button onClick={closeModal}>
+                    <FontAwesomeIcon icon={faClose} size="2x" />
+                  </button>
+                </div>
+                <div className="personal-info-edit-1">
+                  <h2>Personal information</h2>
+                  <div className="personal-infoPartOne">
+                    <div>
+                      <input type="text" placeholder="First name" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Last name" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Email" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Password" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Comfirm password" />
+                    </div>
                   </div>
                 </div>
                 <div className="address-info-edit">
-                  <div>
-                    <span>City : </span>
-                    <input type="text" />
+                  <h2>Address information</h2>
+                  <div className="personal-infoPartTwo">
+                    <div>
+                      <input type="text" placeholder="City" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Subcity" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="Woreda" />
+                    </div>
+                    <div>
+                      <input type="text" placeholder="SSN" />
+                    </div>
                   </div>
-                  <div>
-                    <span>Subcity : </span>
-                    <input type="text" />
-                  </div>
-                  <div>
-                    <span>Woreda : </span>
-                    <input type="text" />
-                  </div>
-                  <div>
-                    <span>SSN : </span>
-                    <input type="text" />
-                  </div>
+                </div>
+                <div className="infoEdit-btns">
+                  <button>Save</button>
+                  <button>Cancel</button>
                 </div>
               </div>
             </UploadModal>
