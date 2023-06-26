fb28449bb9dc094bc19c67f8cb866b3ccd8b2dc7
Biniyam Zeleke <58305735+Binize@users.noreply.github.com>
2023-06-27 01:17:29 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ import UserMessagePage from "./components/HomePage/UserMessagePage";
 import AdminDashboard from "./components/AdminPage/AdminDashboard";
 import AdminDeliveryRequest from "./components/AdminPage/AdminDeliveryRequest";
 import ManageAccount from "./components/AdminPage/ManageAccount";
-
+import termsandconditions from "./termsconditions"
 Modal.setAppElement("#root");
 
 function App() {
